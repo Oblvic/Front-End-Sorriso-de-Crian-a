6c284cc6c9f5b4ba58452570da532cd8c5f2bf46
Tanegashima <135833574+Oblvic@users.noreply.github.com>
2024-06-13 20:10:57 +0000

    ++comentarios

--- SORRISO-DE-CRIAN-A/src/components/schemas/signinSchema.jsx
+++ SORRISO-DE-CRIAN-A/src/components/schemas/signinSchema.jsx
@@ -1,8 +1,9 @@
-import { z } from "zod";
+import { z } from "zod"; // Importando Zod para validação de esquema
 
+// Definindo esquema de validação para o formulário de login
 export const signinSchema = z.object({
-  email: z.string().email({ message: "E-mail inválido" }).toLowerCase(),
-  password: z
+  email: z.string().email({ message: "E-mail inválido" }).toLowerCase(), // Campo de e-mail com validação de e-mail
+  password: z // Campo de senha
     .string()
-    .min(6, { message: "Senha deve ter no mínimo 6 caracteres" }),
+    .min(6, { message: "Senha deve ter no mínimo 6 caracteres" }), // Verificando o comprimento mínimo da senha
 });
--- SORRISO-DE-CRIAN-A/src/components/schemas/signupSchema.jsx
+++ SORRISO-DE-CRIAN-A/src/components/schemas/signupSchema.jsx
@@ -1,18 +1,22 @@
-import { z } from "zod";
+import { z } from "zod"; // Importando Zod para validação de esquema
 
+// Definindo esquema de validação para o formulário de cadastro
 export const signupSchema = z.object({
-  name: z
+  name: z // Campo de nome
     .string()
-    .min(3, { message: "O nome deve ter no mínimo 3 caracteres" })
-    .transform((name) =>
-      name
-        .trim()
-        .split(" ")
-        .map((word) => word[0].toUpperCase() + word.slice(1))
-        .join(" "),
+    .min(3, { message: "O nome deve ter no mínimo 3 caracteres" }) // Verificando o comprimento mínimo do nome
+    .transform(
+      (
+        name, // Transformando o nome para capitalizar as palavras
+      ) =>
+        name
+          .trim() // Removendo espaços em branco no início e no final
+          .split(" ") // Dividindo o nome em palavras
+          .map((word) => word[0].toUpperCase() + word.slice(1)) // Capitalizando a primeira letra de cada palavra
+          .join(" "), // Juntando as palavras novamente em uma string
     ),
-  email: z.string().email({ message: "E-mail inválido" }).toLowerCase(),
-  password: z
+  email: z.string().email({ message: "E-mail inválido" }).toLowerCase(), // Campo de e-mail com validação de e-mail
+  password: z // Campo de senha
     .string()
-    .min(6, { message: "A senha precisa ter no mínimo 6 caracteres" }),
+    .min(6, { message: "A senha precisa ter no mínimo 6 caracteres" }), // Verificando o comprimento mínimo da senha
 });
